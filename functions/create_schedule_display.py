8fc46ebe68de4a08ee6dbdcc5947c00fbb63a9ce
CoalPlays <coalcrafter202@gmail.com>
2023-10-23 16:21:30 +0200

    adds pages to data table

--- functions/create_schedule_display.py
+++ functions/create_schedule_display.py
@@ -13,6 +13,8 @@ import subprocess
 from .create_time_picker import create_time_picker
 from services import TranslateService, LanguageConfiguration
 
+schedule_page = 0
+
 def create_schedule_display(schedule, participant_info, teststarter, isHab = False):
 
     language_config = LanguageConfiguration()
@@ -54,6 +56,8 @@ def create_schedule_display(schedule, participant_info, teststarter, isHab = Fal
     clock = pygame.time.Clock()
     
     def draw_button(screen, message, x, y, w, h, ic, ac, action=None):
+        global button_clicked
+
         # This is the function declaration, which includes the parameters: 
         # screen (the Pygame display surface), 
         # message (the text to be displayed on the button), 
@@ -281,6 +285,29 @@ def create_schedule_display(schedule, participant_info, teststarter, isHab = Fal
         # Destroy the root window
         root.destroy()
     
+    def split_dict(input_dict, chunk_size):
+        dict_list = [{}]
+        current_dict = 0
+
+        for key, value in input_dict.items():
+            dict_list[current_dict][key] = value
+            if len(dict_list[current_dict]) >= chunk_size:
+                dict_list.append({})
+                current_dict += 1
+
+        return dict_list
+    
+
+    splitted_schedule = split_dict(schedule, 30)
+
+    def page_update(schedule, increment):
+        pythonTime.sleep(0.125)
+        global schedule_page
+        if increment:
+            schedule_page = (schedule_page + 1) % len(splitted_schedule)
+        else:
+            schedule_page = (schedule_page - 1) if schedule_page > 0 else len(splitted_schedule) - 1
+
     # Calculate column widths and row height based on screen size
     column_width = screen_width // 9
     column_start_x = screen_width - (column_width * 4) - 50  # Calculate column positions based on screen size (adjusted for right alignment)
@@ -328,14 +355,13 @@ def create_schedule_display(schedule, participant_info, teststarter, isHab = Fal
     if isHab:
         todo_row_multiplicator = 1
     while True:
-        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 mouse_pos = pygame.mouse.get_pos()
-                for row in range(1, len(schedule) + 1):
+                for row in range(1, len(splitted_schedule[schedule_page]) + 1):
                     if not isHab:
                         newdate_input_box_rect = pygame.Rect(
                             column_start_x + (column_width),
@@ -357,7 +383,7 @@ def create_schedule_display(schedule, participant_info, teststarter, isHab = Fal
                     )
                     if todo_input_box_rect.collidepoint(mouse_pos):
                         todo_active_row = row
-                        todo_input_values[row] = state_iterator[schedule[list(schedule)[row-1]]["state"]]
+                        todo_input_values[row] = state_iterator[splitted_schedule[schedule_page][list(splitted_schedule[schedule_page])[row-1]]["state"]]
                         active_column = "todo"  # Set the active column
                         todo_input_active = True
                         newtime_input_active = False
@@ -367,7 +393,7 @@ def create_schedule_display(schedule, participant_info, teststarter, isHab = Fal
                     if not isHab:
                         if newdate_input_box_rect.collidepoint(mouse_pos) and not isHab:
                             newdate_active_row = row
-                            newdate_input_values[row] = schedule[list(schedule)[row-1]]["datetime"].split(" ")[0]
+                            newdate_input_values[row] = splitted_schedule[schedule_page][list(splitted_schedule[schedule_page])[row-1]]["datetime"].split(" ")[0]
                             active_column = "newdate"  # Set the active column
                             newtimedate_input_active = False
                             newdate_input_active = True
@@ -385,7 +411,7 @@ def create_schedule_display(schedule, participant_info, teststarter, isHab = Fal
                             todo_input_active = False
                             newdate_active_row = None
                             newdate_input_active = False
-                            current_time = schedule[list(schedule)[row-1]]["datetime"].split(" ")[1]
+                            current_time = splitted_schedule[schedule_page][list(splitted_schedule[schedule_page])[row-1]]["datetime"].split(" ")[1]
                             splitted_time = current_time.split(":")
                             time_picker = create_time_picker(splitted_time[0], splitted_time[1], translate_service)
                             formatted_time = str(time_picker.time()[0]).rjust(2, '0') + ":" + str(time_picker.time()[1]).rjust(2, '0') +":00"
@@ -413,11 +439,17 @@ def create_schedule_display(schedule, participant_info, teststarter, isHab = Fal
         draw_button(screen, translate_service.get_translation("help"), 50 * width_scale_factor, 400 * height_scale_factor, 200 * width_scale_factor, 50 * height_scale_factor, light_grey, light_grey, button_help)
         draw_button(screen, translate_service.get_translation("english"), 50 * width_scale_factor, 500 * height_scale_factor, 70 * width_scale_factor, 50 * height_scale_factor, light_grey, light_grey, lambda: change_language(translate_service, language_config, "en"))
         draw_button(screen, translate_service.get_translation("german"), 180 * width_scale_factor, 500 * height_scale_factor, 70 * width_scale_factor, 50 * height_scale_factor, light_grey, light_grey, lambda: change_language(translate_service, language_config, "de"))
+        
+
+        draw_button(screen, "<", column_start_x + 1.5 * column_width, 775 * height_scale_factor, 10 * width_scale_factor, 10 * height_scale_factor, light_grey, light_grey, lambda: page_update(splitted_schedule, False))
+        draw_button(screen, ">", column_start_x + 2.5 * column_width, 775 * height_scale_factor, 10 * width_scale_factor, 10 * height_scale_factor, light_grey, light_grey, lambda: page_update(splitted_schedule, True))
 
         # Display column headers with adjusted font size
         font = pygame.font.Font(None, int(20 * width_scale_factor)) # Create font object for header
         text_surface = font.render(' ' + translate_service.get_translation("task"), True, light_grey) # Render the text 'Task' with the font and color light_grey
         screen.blit(text_surface, (column_start_x, cellPadding)) # Blit the text surface to the screen at the specified position
+        page_number_surface = font.render(str(schedule_page + 1) + "/" + str(len(splitted_schedule)) , True, light_grey)
+        screen.blit(page_number_surface, (column_start_x + 2 * column_width, 775 * height_scale_factor))
         if not isHab:
             text_surface = font.render(' ' + translate_service.get_translation("date"), True, light_grey) # Render the text 'Time'
             screen.blit(text_surface, (column_start_x + column_width, cellPadding)) # Blit the text surface to the screen
@@ -432,7 +464,8 @@ def create_schedule_display(schedule, participant_info, teststarter, isHab = Fal
         font = pygame.font.Font(None, int(16 * width_scale_factor))
         row = 1  # Initialize the row counter
         # Loop through each task and datetime in the schedule dictionary
-        for task, info in schedule.items():
+        
+        for task, info in splitted_schedule[schedule_page].items():
             date = info["datetime"].split(" ")[0]
             time = info["datetime"].split(" ")[1]
             state = info["state"]
@@ -483,7 +516,7 @@ def create_schedule_display(schedule, participant_info, teststarter, isHab = Fal
                         newdate_input_text_surface = newdate_input_box_font.render(newdate_input_value, True, red)
                     else:
                         newdate_input_text_surface = newdate_input_box_font.render(newdate_input_value, True, light_grey)
-                        schedule[list(schedule)[row-1]]["datetime"] = newdate_input_value + " " + time
+                        splitted_schedule[schedule_page][list(splitted_schedule[schedule_page])[row-1]]["datetime"] = newdate_input_value + " " + time
                     screen.blit(newdate_input_text_surface, newdate_input_box_rect.move(5, 5))
                 elif newdate_active_row == row:
                     newdate_input_text_surface = newdate_input_box_font.render(newdate_input_text, True, light_grey)
@@ -499,7 +532,7 @@ def create_schedule_display(schedule, participant_info, teststarter, isHab = Fal
                         newtime_input_text_surface = newtime_input_box_font.render(newtime_input_value, True, red)
                     else:
                         newtime_input_text_surface = newtime_input_box_font.render(newtime_input_value, True, light_grey)
-                        schedule[list(schedule)[row-1]]["datetime"] = date + " " + newtime_input_value
+                        splitted_schedule[schedule_page][list(splitted_schedule[schedule_page])[row-1]]["datetime"] = date + " " + newtime_input_value
                     screen.blit(newtime_input_text_surface, newtime_input_box_rect.move(5, 5))
                 elif newtime_active_row == row:
                     newtime_input_text_surface = newtime_input_box_font.render(newtime_input_text, True, light_grey)
@@ -514,7 +547,7 @@ def create_schedule_display(schedule, participant_info, teststarter, isHab = Fal
                 todo_input_text_surface = todo_input_box_font.render(translate_service.get_translation(todo_input_value), True, todo_color[todo_input_value.lower()])
 
                 if todo_input_value.lower() == "todo" or todo_input_value.lower() == "done" or todo_input_value.lower() == "skip":
-                    schedule[list(schedule)[row-1]]["state"] = todo_input_value.lower()
+                    splitted_schedule[schedule_page][list(splitted_schedule[schedule_page])[row-1]]["state"] = todo_input_value.lower()
                 else:
                     todo_input_text_surface = todo_input_box_font.render(todo_input_value, True, red)
                 screen.blit(todo_input_text_surface, todo_input_box_rect.move(5, 5))
@@ -522,7 +555,7 @@ def create_schedule_display(schedule, participant_info, teststarter, isHab = Fal
                 todo_input_text_surface = todo_input_box_font.render(todo_input_text, True, todo_color[todo_input_values[row].lower()])
                 screen.blit(todo_input_text_surface, todo_input_box_rect.move(5, 5))
             elif todo_active_row != row:
-                todo_input_text_surface = todo_input_box_font.render(translate_service.get_translation(state), True, todo_color[schedule[list(schedule)[row-1]]["state"].lower()])
+                todo_input_text_surface = todo_input_box_font.render(translate_service.get_translation(state), True, todo_color[splitted_schedule[schedule_page][list(splitted_schedule[schedule_page])[row-1]]["state"].lower()])
                 screen.blit(todo_input_text_surface, todo_input_box_rect.move(5, 5))
 
             # Render the cursor
@@ -543,7 +576,9 @@ def create_schedule_display(schedule, participant_info, teststarter, isHab = Fal
         
             row += 1  # Increment the row counter
             
-            final_y = (len(schedule) + 1) * row_height
+            max_grid_rows = 30 if len(schedule) > 30 else len(schedule)
+
+            final_y = (max_grid_rows + 1) * row_height
 
             # Draw grid lines
             grid_range = 5 if not isHab else 3
@@ -551,7 +586,7 @@ def create_schedule_display(schedule, participant_info, teststarter, isHab = Fal
                 x = column_start_x + (i * column_width)
                 pygame.draw.line(screen, light_grey, (x, 0), (x, final_y), 1)
 
-            for i in range(1, len(schedule) + 2):
+            for i in range(1, max_grid_rows + 2):
                 y = i * row_height
                 pygame.draw.line(screen, light_grey, (column_start_x, y), (column_start_x + ((grid_range -1) * column_width), y), 1)
 
