cc3bcbc4e1dcdf6cbcf370c25389e3a2010e1879
CoalPlays <coalcrafter202@gmail.com>
2023-11-09 16:01:44 +0100

    Edits input box By holding down backspace text will be removed slowly Added icon to copy text in text box

--- classes/InputBox.py
+++ classes/InputBox.py
@@ -1,12 +1,12 @@
 import pygame
 from pygame.locals import *
-
+from datetime import datetime
 class InputBox:
     def __init__(self, x, y, width, height, translation_key, translate_service, info="", initial_text="", allow_new_line = False, not_allowed_characters = []):
         self.translate_service = translate_service
         self.rect = pygame.Rect(x - width // 2, y, width, height)
         self.color = pygame.Color("gray")
-        self.active_color = pygame.Color("gray")
+        self.active_color = (218, 221, 220)
         self.text_color = pygame.Color("black")
         self.label_color = pygame.Color("gray")
         self.active_text_color = pygame.Color("black")
@@ -20,6 +20,14 @@ class InputBox:
         self.cursor_timer = 0
         self.allow_new_line = allow_new_line
         self.not_allowed_characters = not_allowed_characters
+        self.started_removing = False
+        self.delay = None
+        self.image = pygame.image.load("./img/copyIcon.png")
+        self.posX = x
+        self.posY = y
+        self.imagePos = pygame.Rect(0,0, self.image.get_rect().width, self.image.get_rect().height)
+
+
 
     def handle_event(self, event):
         if event.type == MOUSEBUTTONDOWN:
@@ -27,6 +35,13 @@ class InputBox:
                 self.is_selected = True
             else:
                 self.is_selected = False
+            if self.imagePos.collidepoint(pygame.mouse.get_pos()) and event.button == 1:
+                pygame.scrap.put(pygame.SCRAP_TEXT, self.text.encode('utf-8'))
+        elif event.type == KEYUP:
+            if self.is_selected:
+                if event.key == K_BACKSPACE:
+                    self.started_removing = False
+                    self.delay = None
         elif event.type == KEYDOWN:
             mods = pygame.key.get_mods()
             if self.is_selected:
@@ -44,11 +59,15 @@ class InputBox:
                                 print("Error: Unable to decode clipboard content.")
                         else:
                             print("Error: Unable to retrieve clipboard content.")
+                    elif event.key == K_BACKSPACE:
+                        self.text = ""
 
                 elif event.key == K_RETURN:
                     pass
                 elif event.key == K_BACKSPACE:
                     self.text = self.text[:-1]
+                    self.started_removing = True
+                    self.delay = -10
                 elif event.key == K_TAB:
                     pass
                 else:
@@ -64,9 +83,14 @@ class InputBox:
         pygame.draw.rect(screen, self.active_color if self.is_selected else self.color, self.rect)
         input_text = self.text
         text_surface = self.font.render(input_text, True, self.active_text_color if self.is_selected else self.text_color)
-        while text_surface.get_rect().width > self.rect.width -10:
+        while text_surface.get_rect().width > self.rect.width // 100 * 88:
             input_text = input_text[1:]
             text_surface = self.font.render(input_text, True, self.active_text_color if self.is_selected else self.text_color)
+        if self.is_selected and self.delay != None and -20 < self.delay < 16:
+            if self.delay == 15:
+                self.text = self.text[:-1]
+                self.delay = 10
+            self.delay += 1
 
         screen.blit(text_surface, (self.rect.x + 5, self.rect.y + 5))
         screen.blit(self.label, (self.rect.x - self.label.get_width() - 10, self.rect.y + 5))
@@ -80,3 +104,6 @@ class InputBox:
                 pygame.draw.line(screen, pygame.Color("black"),
                                  (self.rect.x + text_surface.get_width() + 5, self.rect.y + 5),
                                  (self.rect.x + text_surface.get_width() + 5, self.rect.y + self.rect.height - 5))
+        self.imagePos = pygame.Rect(self.posX + (self.rect.width// 2) // 100 * 80, self.posY, self.image.get_rect().width, self.image.get_rect().height)
+        screen.blit(self.image, (self.posX + (self.rect.width// 2) // 100 * 80, self.posY ))
+        
