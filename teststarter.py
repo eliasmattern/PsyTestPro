593a15c67b5840ae302068d3f1def94e57ebf476
CoalPlays <coalcrafter202@gmail.com>
2023-11-15 14:29:54 +0100

    renames functions folder to displays folder

--- teststarter.py
+++ teststarter.py
@@ -7,7 +7,7 @@ import sys
 from pygame.locals import *
 from datetime import datetime, timedelta
 from classes import InputBox, Button
-from functions import create_schedule_display, ExperimentConfigDisplay
+from displays import create_schedule_display, ExperimentConfigDisplay
 import re
 from services import TranslateService, LanguageConfiguration, TeststarterConfig
 
