8df333971a9363520ae66e5bd78c51a7325123c4
eliasmattern <coalcrafter202@gmail.com>
2024-06-18 12:37:09 +0200

    Checks if window is focused during task

--- services/execute_command_service.py
+++ services/execute_command_service.py
@@ -1,21 +1,41 @@
 import shlex
 import subprocess
 
+import pygame.event
 
-def execute_command(command: str, participant_info: dict, variables: dict) -> tuple[bytes, int]:
-    command = command.format(id=participant_info['participant_id'],
-                                                            experiment=participant_info['suite'],
-                                                            startTime=participant_info['start_time'],
-                                                            timestamp=participant_info['timestamp'],
-                                                            scriptCount=str(participant_info['script_count']),
-                                                            **variables)
+
+def execute_command(command: str, participant_info: dict, variables: dict) -> tuple[int, int]:
+    command = command.format(id='"' + participant_info['participant_id'] + '"',
+                             experiment='"' + participant_info['suite'] + '"',
+                             startTime=participant_info['start_time'],
+                             timestamp=participant_info['timestamp'],
+                             scriptCount=str(participant_info['script_count']),
+                             **variables)
     try:
         process = subprocess.Popen(shlex.split(command, posix=False), shell=False)
-        output, error = process.communicate()
+        error = 0
+        is_focused = True
+        while process.poll() is None or not is_focused:
+            if process.stderr:
+                error += 1
+            for event in pygame.event.get():
+                if event.type == pygame.WINDOWFOCUSLOST:
+                    is_focused = False
+                elif event.type == pygame.WINDOWFOCUSGAINED:
+                    is_focused = True
         return_code = process.wait()
         return error, return_code
     except Exception as _:
         process = subprocess.Popen(shlex.split(command, posix=False), shell=True)
-        output, error = process.communicate()
+        error = 0
+        is_focused = True
+        while process.poll() is None or not is_focused:
+            if process.stderr:
+                error += 1
+            for event in pygame.event.get():
+                if event.type == pygame.WINDOWFOCUSLOST:
+                    is_focused = False
+                elif event.type == pygame.WINDOWFOCUSGAINED:
+                    is_focused = True
         return_code = process.wait()
         return error, return_code
