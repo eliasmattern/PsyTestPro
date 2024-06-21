522de7275c5f26d98de6267341654f49ab416d55
eliasmattern <coalcrafter202@gmail.com>
2024-06-21 11:18:36 +0200

    Adds comments to PathService

--- services/PathService.py
+++ services/PathService.py
@@ -2,8 +2,13 @@ import sys
 import os
 
 
-def get_resource_path(relative_path: str, outside: bool = False):
-    """ Get absolute path to resource, works for dev and for PyInstaller """
+def get_resource_path(relative_path: str, outside: bool = False) -> str:
+    """ Returns absolute path. Needed to locate files after packaging PsyTestPro to executeable.
+    :param relative_path: str
+    :param outside: boolean -> If the file should be located next to the executable and not in the internal dir
+    :return:
+        absolute_path: str
+    """
     try:
         # PyInstaller creates a temp folder and stores path in _MEIPASS
         base_path = sys._MEIPASS
