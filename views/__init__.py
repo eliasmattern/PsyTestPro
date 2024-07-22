e97cd400b6f5b62a88bc8f3733c8adf1a402a7e7
eliasmattern <coalcrafter202@gmail.com>
2024-07-22 11:34:30 +0200

    Adds view to add existing task to a suite

--- views/__init__.py
+++ views/__init__.py
@@ -2,3 +2,4 @@ from .create_schedule_display import CreateScheduleDisplay
 from .suite_config_view import SuiteConfig
 from .settings_view import SettingsView
 from .task_create_group_view import CreateTaskGroupView
+from .add_existing_task_view import AddExistingTaskView
