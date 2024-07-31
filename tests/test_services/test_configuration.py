910863d3e4386b2862bc42c3d42354c7f71e904a
eliasmattern <coalcrafter202@gmail.com>
2024-07-31 14:42:18 +0200

    adds test_load_task_of_suite

--- tests/test_services/test_configuration.py
+++ tests/test_services/test_configuration.py
@@ -1,8 +1,9 @@
 import unittest
 from unittest.mock import patch, mock_open
 
+from app_types import Task, TaskGroup
 from services import PsyTestProConfig
-from tests.test_services.test_services_utils import TASK_CONFIG_JSON, TASK_FROM_CONFIG
+from tests.test_services.test_services_utils import TASK_CONFIG_JSON, TASK_FROM_CONFIG, GROUP_FROM_CONFIG
 
 
 class ConfigurationTests(unittest.TestCase):
@@ -37,7 +38,7 @@ class ConfigurationTests(unittest.TestCase):
 		mock_json_load.assert_called_once_with(mock_open())
 
 		self.assertEqual(self.psy_test_pro_config.current_suite, 'suite_schedule')
-		self.assertEqual(len(self.psy_test_pro_config.current_tasks), 1)
+		self.assertGreater(len(self.psy_test_pro_config.current_tasks), 0)
 		task = self.psy_test_pro_config.current_tasks[0]
 
 		self.assertEqual([task.id, task.name, task.task_type, task.duration, task.value],
@@ -54,7 +55,35 @@ class ConfigurationTests(unittest.TestCase):
 
 		result = self.psy_test_pro_config.get_suites()
 
+		mock_get_resource_path.assert_called_once_with('json/taskConfig.json')
 		self.assertEqual(result, list(tasks.keys()))
+
+	@patch('services.configuration.get_resource_path', return_value='mock.json/taskConfig.json')
+	@patch('builtins.open', new_callable=mock_open, read_data='file')
+	@patch('json.load')
+	def test_load_task_of_suite(self, mock_json_load, mock_open, mock_get_resource_path):
+		mock_json_load.return_value = TASK_CONFIG_JSON.copy()
+		result = self.psy_test_pro_config.load_task_of_suite('suite_schedule')
+
+		mock_get_resource_path.assert_called_once_with('json/taskConfig.json')
+		mock_open.assert_called_once_with('mock.json/taskConfig.json', 'r',
+										  encoding='utf-8')
+		mock_json_load.assert_called_once_with(mock_open())
+
+		self.assertEqual(len(result), 2)
+		self.assertIsInstance(result[0], Task)
+		self.assertIsInstance(result[1], TaskGroup)
+
+		task = result[0]
+		task_group = result[1]
+		self.assertEqual([task.id, task.name, task.task_type, task.duration, task.value],
+						 [TASK_FROM_CONFIG.id, TASK_FROM_CONFIG.name, TASK_FROM_CONFIG.task_type,
+						  TASK_FROM_CONFIG.duration, TASK_FROM_CONFIG.value])
+		self.assertEqual([task_group.id, task_group.name, task_group.pause_inbetween, task_group.loops],
+						 [GROUP_FROM_CONFIG.id, GROUP_FROM_CONFIG.name, GROUP_FROM_CONFIG.pause_inbetween,
+						  GROUP_FROM_CONFIG.loops])
+
+		self.assertEqual(len(task_group.tasks), len(GROUP_FROM_CONFIG.tasks))
 
 
 	if __name__ == '__main__':
--- tests/test_services/test_services_utils.py
+++ tests/test_services/test_services_utils.py
@@ -1,4 +1,4 @@
-from app_types import Task
+from app_types import Task, TaskGroup
 
 TASK_CONFIG_JSON = {
 	"globalTasks": {
@@ -51,31 +51,36 @@ TASK_CONFIG_JSON = {
 				"value": {
 					"title": "Title",
 					"description": "Description"
-				},
-				"1": {
-					"is_group": True,
-					"name": "GROUP",
-					"pause": "00:02:00",
-					"loops": 2,
-					"tasks": {
-						"0": {
-							"is_group": False,
-							"name": "TASKOFGROUP",
-							"time": "00:10:00",
-							"state": "todo",
-							"type": "text",
-							"value": {
-								"title": "TITLE",
-								"description": "DESCRIPTION"
-							},
-							"position": 1
-						}
-					},
-					"position": 2
 				}
+
+			},
+			"1": {
+				"is_group": True,
+				"name": "GROUP",
+				"pause": "00:02:00",
+				"loops": 2,
+				"tasks": {
+					"0": {
+						"is_group": False,
+						"name": "TASKOFGROUP",
+						"time": "00:10:00",
+						"state": "todo",
+						"type": "text",
+						"value": {
+							"title": "TITLE",
+							"description": "DESCRIPTION"
+						},
+						"position": 1
+					}
+				},
+				"position": 2
 			}
 		}
 	}
 }
 
 TASK_FROM_CONFIG = Task('0', 'TESTTASK', '00:05:00', 'text', {"title": "Title", "description": "Description"}, 1)
+
+GROUP_FROM_CONFIG = TaskGroup('1', 'GROUP', '00:02:00', 2, {
+	'0': {'name': 'TESTTASK', 'time': '00:05:00', 'type': 'text',
+		  'value': {"title": "Title", "description": "Description"}, 'state': 'todo', 'position': 1}}, 2)
