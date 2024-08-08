061107106236fbb4829e3c545961119cf07d13b3
eliasmattern <coalcrafter202@gmail.com>
2024-08-08 13:52:00 +0200

    Adds fall back language if user tries to access a language which is not loaded

--- services/TranslateService.py
+++ services/TranslateService.py
@@ -23,7 +23,7 @@ class TranslateService:
 			raise Exception(f'Translation file for {language} not found')
 
 	def get_translation(self, key: str):
-		return self.translations[self.current_language].get(key, self.translations['en'].get(key, ''))
+		return self.translations.get(self.current_language, 'en').get(key, self.translations['en'].get(key, ''))
 
 	def set_language(self, language: str):
 		if language not in self.translations:
